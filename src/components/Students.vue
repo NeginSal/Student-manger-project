d07326813b59cde38cfaeaf84b1679d754c32635
NeginSal <neginsaljooghi@gmail.com>
2022-05-21 17:06:07 +0430

    first commit

---
+++ src/components/Students.vue
@@ -0,0 +1,29 @@
+<template>
+ <v-flex sm8 offset-sm2>
+    <v-card>
+      <v-toolbar dark>
+        <v-toolbar-title>Students</v-toolbar-title>
+      </v-toolbar>
+      <v-list>
+        <v-list-tile v-for="(student, index) in $store.state.students" :to="'editStudent/' + index" :key="student.id">
+          <v-list-tile-content>
+            <v-list-tile-title v-text="student.name"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    data() {
+        return {
+        }
+    },
+  async created() {
+  }
+};
+</script>
