4996e9a35e73f6c65650c4a392d24465801e3877
NeginSal <neginsaljooghi@gmail.com>
2022-05-25 17:12:55 +0430

    using mapState

--- src/components/Students.vue
+++ src/components/Students.vue
@@ -1,11 +1,15 @@
 <template>
- <v-flex sm8 offset-sm2>
+  <v-flex sm8 offset-sm2>
     <v-card>
       <v-toolbar dark>
         <v-toolbar-title>Students</v-toolbar-title>
       </v-toolbar>
       <v-list>
-        <v-list-tile v-for="(student, index) in $store.state.students" :to="'editStudent/' + index" :key="student.id">
+        <v-list-tile
+          v-for="(student, index) in $store.state.students"
+          :to="'editStudent/' + index"
+          :key="student.id"
+        >
           <v-list-tile-content>
             <v-list-tile-title v-text="student.name"></v-list-tile-title>
           </v-list-tile-content>
@@ -17,15 +21,15 @@
 
 <script>
 import axios from "axios";
-import store from '../store'
+import { mapState } from "vuex";
 
 export default {
-    data() {
-        return {
-          students: store.state.students
-        }
-    },
-  async created() {
-  }
+  data() {
+    return {};
+  },
+  computed: mapState({
+    students: (state) => state.students,
+  }),
+  async created() {},
 };
 </script>
