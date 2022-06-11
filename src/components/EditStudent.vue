579bdd6b0bc7dca241bd9012011940419f328544
NeginSal <neginsaljooghi@gmail.com>
2022-06-11 18:26:40 +0430

    updating edit student with actions-2

--- src/components/EditStudent.vue
+++ src/components/EditStudent.vue
@@ -19,12 +19,14 @@
             <v-layout>
               <v-flex xs12 md4>
                 <v-text-field
-                  v-model="findStudent($route.params.id).firstName"
+                  @input="updateFirstName"
+                  :value="findStudent($route.params.id).firstName"
                   label="first Name"
                   required
                 ></v-text-field>
                 <v-text-field
-                  v-model="findStudent($route.params.id).lastName"
+                  @input="updateLastName"
+                  :value="findStudent($route.params.id).lastName"
                   label="last Name"
                   required
                 ></v-text-field>
@@ -47,22 +49,35 @@ import { mapGetters } from "vuex";
 
 export default {
   data() {
-    return {};
+    return {
+      firstName: "",
+      lastName: "",
+    };
   },
   computed: {
     // student() {
     //   return this.$store.getters.findStudent(this.$route.params.id);
     // },
-    ...mapGetters(['isLoaded','findStudent'])
+    ...mapGetters(["isLoaded", "findStudent"]),
   },
   methods: {
     async submit() {
-      const firstName =this.findStudent(this.$route.params.id).firstName;
-       const lasttName =this.findStudent(this.$route.params.id).lastName;
+      const student = this.findStudent(this.$route.params.id)
 
-      this.$store.dispatch('editStudent',{id:this.$route.params.id,names:{firstName,lasttName}})
+      const firstName = this.firstName || student.firstName;
+      const lastName = this.lastName ||student.lastName;
 
-    }
+      this.$store.dispatch("editStudent", {
+        id: this.$route.params.id,
+        names: { firstName, lastName},
+      });
+    },
+    updateFirstName(e) {
+      this.firstName = e;
+    },
+    updateLirstName(e) {
+      this.lastName = e;
+    },
   },
   components: {
     Students,
